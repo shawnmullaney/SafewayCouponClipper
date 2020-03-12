327abf222039e16e8b07e0360bd7684827457148
Shawn Mullaney <60525909+shawnmullaney@users.noreply.github.com>
2020-03-11 17:23:43 -0700

    new version of j4u. new login methods and cleaned up lists

--- j4u.py
+++ j4u.py
@@ -3,6 +3,9 @@
 ### and a WebDriver utility('http://chromedriver.chromium.org/downloads'). I Chose Chrome. 
 ## Maybe I will create a windows installer!!!!!
 # maybe.
+#localStorage.getItem('abCoupons')
+#browser.execute_script("localStorage.getItem('abCoupons'))
+#document.getElementById('couponAddBtn*').dispatchEvent(new MouseEvent("click"));
 ######## JavaScript snippet borrowed from "https://github.com/kton/Safeway-Just-for-u"
 import os
 import js2py
@@ -12,25 +15,75 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
 
-usernameStr = 'email'
-passwordStr = 'password'
+
+usernameStr = 'shawnmullaney@hotmail.com'
+passwordStr = 'poopoo11'
 ##
 chrome_options = Options()  
-chrome_options.add_argument("--headless")
-browser = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"),   chrome_options=chrome_options) 
-##
+#chrome_options.add_argument("--headless")
+browser = webdriver.Chrome(executable_path=os.path.abspath("/usr/local/bin/chromedriver"),   options=chrome_options) 
+urldeals = '''https://www.safeway.com/content/www/(banner)/en/justforu/coupons-deals.html'''
+browser.get(('https://www.safeway.com/content/www/(banner)/en/justforu/coupons-deals.html'))
+#browser.get(('https://www.safeway.com/CMS/account/login/?FullSite=Y&goto=https:%2F%2Fwww.safeway.com%2FShopStores%2FjustforU.page'))
 
-#browser = webdriver.Chrome()
-browser.get(('https://www.safeway.com/CMS/account/login/?FullSite=Y&goto=https:%2F%2Fwww.safeway.com%2FShopStores%2FjustforU.page'))
 js_crap = '''javascript:(function() {"use strict";var coupons = angular.element("#lt-coupon-area").scope().sharedValues.unfilteredItems.slice(); var token = localStorage.getItem("oktathenticateAccessToken"); if (token && coupons) { coupons.filter(function(x){return x.clipStatus==="U";}).forEach(function(item){ var data = {"items":[]}; var clip = {}; clip.clipType="C";clip.itemId=item.offerId;clip.itemType=item.offerPgm;clip.vndrBannerCd=""; var list = {}; list.clipType="L";list.itemId=item.offerId;list.itemType=item.offerPgm; data.items.push(clip);data.items.push(list); var request = new Request('https://nimbus.safeway.com/Clipping1/services/clip/items', { method: 'POST', mode: 'cors', redirect: 'error', headers: new Headers({ 'X-SWY_VERSION': '1.0', 'X-SWY_BANNER': 'safeway', 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json;charset=UTF-8', 'X-SWY_API_KEY': 'emjou', 'X-swyConsumerDirectoryPro': token }), body: JSON.stringify(data) }); fetch(request).then(function(){ document.querySelector("#headerMyListCount").textContent = parseInt(document.querySelector("#headerMyListCount").textContent,10)+1; }); }); } if (!token) { alert('not logged in or a website change broke this again'); }}());'''
-print(js_crap)
-username = browser.find_element_by_id('input-email')
+xpath = '''/html/body/div[1]/div/div/div[1]/div/div/div/div[3]/div[2]/div[1]/div/div/div[3]/ul/li[1]/a/span[1]'''
+x2 = '''//*[@id="sign-in-modal-link"]'''
+#button = browser.find_element_by_xpath('//*[@id="label-email"]')
+#button.click()
+'''
+button = browser.find_element_by_xpath(xpath)
+button.click()
+time.sleep(2)
+button2 = browser.find_element_by_xpath(x2)
+button2.click()
+'''
+time.sleep(2)
+username = browser.find_element_by_id('label-email')
 username.send_keys(usernameStr)
-password = browser.find_element_by_id('password-password')
+time.sleep(4)
+password = browser.find_element_by_id('label-password')
 password.send_keys(passwordStr)
-signInButton = browser.find_element_by_id('create-account-btn') 
+time.sleep(2)
+print("bout to hit sign in button")
+signInButton = browser.find_element_by_id('btnSignIn') 
 signInButton.click()
-print('sleeping for 10 secs..')
-time.sleep(10)
-browser.execute_script(js_crap)
+print("just clicked sign in button")
+
+
+time.sleep(5)
+signInClose = browser.find_element_by_id('onboardingCloseButton') 
+signInClose.click()
+time.sleep(5)
+totaller = 0
+
+def getem():
+    for each in addButton1:
+        each.click()
+        global totaller 
+        totaller = totaller + 1
+        from random import randint
+        from time import sleep
+        sleep(randint(1,10))
+
+addButton1 = browser.find_elements_by_xpath("//*[contains(@id, 'couponAddBtn')]")
+if ! len(addButton1):
+    print("empty list, no more coupons left. Or they caught on hahaha")
+while len(addButton1) > 1:
+    getem() 
+    addButton1 = browser.find_elements_by_xpath("//*[contains(@id, 'couponAddBtn')]")
+    
+
+print(totaller)
+'''
+
+alertMessage = 'clipped a total of' + str(totaller) + ' coupons!!!'
+print(alertMessage)
+
+try:
+    browser.execute_script("alert(alertMessage);")
+except WebDriverException:
+    pass
+'''
